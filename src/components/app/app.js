b0972c8ad1b6e8ac6693a808dc7dfbf51e8134da
dr_cote <merezhin@gmail.com>
2021-12-12 14:04:09 +0300

    Чистый проект

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -23,3 +21,4 @@ function App() {
 }
 
 export default App;
+
